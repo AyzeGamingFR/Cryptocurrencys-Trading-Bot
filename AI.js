463944e30c21a32700430c2e53ed8ddf6b63defe
Dragonlion33ytb <35877385+Dragonlion33ytb@users.noreply.github.com>
2019-10-07 20:31:05 +0200

    Update AI.js

--- AI.js
+++ AI.js
@@ -1,2 +1,10 @@
+///Here is the dependancies
+
+var fs = require("fs");
+
+///Here is all of the datas
+
 var config = require("./datas/config.json");
 var logs = require("./datas/logs.txt");
+
+///Here is the code of the AI
